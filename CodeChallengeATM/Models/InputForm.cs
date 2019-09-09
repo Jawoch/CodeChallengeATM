16ad55b5deea58a4e94e6fd1c275f6cf06a83994
Lukasz Jawoszek - QIAGEN <Lukasz.Jawoszek@qiagen.com>
2019-09-09 19:50:40 +0200

    Hardcoded messages to constans

--- CodeChallengeATM/Models/InputForm.cs
+++ CodeChallengeATM/Models/InputForm.cs
@@ -6,7 +6,7 @@ namespace CodeChallengeATM.Models
     public class InputForm
     {
         [Required]
-        [Range(10, double.MaxValue, ConvertValueInInvariantCulture = false, ErrorMessage = "Requested amount must be positive")]
+        [Range(10, double.MaxValue, ConvertValueInInvariantCulture = false, ErrorMessage = "Requested amount must be positive and grater than 10$")]
         [RegularExpression(@"[0-9]{1,}?0{1}$", ErrorMessage = "Requested amount must be multiples of 10$")]
         public decimal? ImputedAmount { get; set; }
         public List<decimal> Results { get; set; }
--- CodeChallengeATM/Shared/Constants.cs
+++ CodeChallengeATM/Shared/Constants.cs
@@ -4,5 +4,6 @@
     {
         public static readonly int[] Denominations = { 100, 50, 20, 10 };
         public const string AvailableNoteIsException = "Note Unavailable, lowest available note is 10 $";
+        public const string  ProvideNewAmountException = "Provided amount is invalid please provide new amount";
     }
 }
